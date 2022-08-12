a4b20d64e402a767e624b2dad5359d74aa2cca07
GiovaniCappelaro <giovani.c@aluno.ifsp.edu.br>
2022-08-12 18:56:45 -0300

    Classes e atributos main_activity

--- app/src/main/java/com/giovani/cadastro/MainActivity.java
+++ app/src/main/java/com/giovani/cadastro/MainActivity.java
@@ -4,12 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.giovani.cadastro.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding activityMainBinding;
+
+    private String nome = "";
+    private String telefone = "";
+    private String email = "";
+    private boolean aceitoLista = false;
+    private int sexo = 1;
+    private String cidade = "";
+    private String uf = "";
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         setContentView(R.layout.activity_main);
     }
 }
