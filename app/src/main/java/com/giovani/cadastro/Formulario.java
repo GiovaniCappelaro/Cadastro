730385e268408acfcd192d5895796b8208e49fe7
GiovaniCappelaro <giovani.c@aluno.ifsp.edu.br>
2022-08-13 16:03:01 -0300

    Finalização (até testar) classe Formulario

--- app/src/main/java/com/giovani/cadastro/Formulario.java
+++ app/src/main/java/com/giovani/cadastro/Formulario.java
@@ -1,5 +1,35 @@
 package com.giovani.cadastro;
 
 public class Formulario {
-    
+    private String nome;
+    private String telefone;
+    private String email;
+    private boolean aceitoLista;
+    private String sexo;
+    private String cidade;
+    private String uf;
+
+    //constr
+
+    public Formulario(String nome, String telefone, String email, boolean aceitoLista, String sexo, String cidade, String uf) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+        this.aceitoLista = aceitoLista;
+        this.sexo = sexo;
+        this.cidade = cidade;
+        this.uf = uf;
+    }
+
+
+    public String retornaString(){
+        return "Nome: " + nome +
+                "\n\tTelefone: " + telefone +
+                "\n\tEmail: " + email +
+                "\n\tAceitoLista: " + aceitoLista +
+                "\n\tSexo: " + sexo +
+                "\n\tCidade: " + cidade +
+                " - " + uf ;
+    }
+
 }
